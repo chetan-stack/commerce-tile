c6f3a1c9c96a557182a6f18c46f214acb7bf1d72
chetan-stack <66368830+chetan-stack@users.noreply.github.com>
2024-03-04 14:22:58 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TecnologyComponent } from './tecnology/tecnology.component';
 import { AddToCardComponent } from './add-to-card/add-to-card.component';
 import { FooterComponent } from './includes/header/footer/footer.component';
 import { FooterWhiteComponent } from './includes/footer-white/footer-white.component';
+import { NgxUiLoaderModule, NgxUiLoaderRouterModule } from 'ngx-ui-loader';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import { FooterWhiteComponent } from './includes/footer-white/footer-white.compo
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgxUiLoaderModule,
+    NgxUiLoaderRouterModule
 
   ],
   providers: [],
