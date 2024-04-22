4d761bb7ff2e3b166aff41b59308163de2e5878b
chetan-stack <66368830+chetan-stack@users.noreply.github.com>
2024-04-22 11:52:57 +0530

    update pdf

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
+
+  constructor(){
+
+  }
+
+  emailsend(data:any){
+    console.log('data')
+    if(data.invalid){
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Filled All Mandatory Feilds",
+      });
+    }
+    else{
+     
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "we have recieved your query. We will get back to you shortly",
+      });
+    }
+  }
 
 }
